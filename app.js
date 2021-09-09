90e87e620f3bdf47913b1a4912c36b5a4b576576
DukeTwoCan <duketoucan@gmail.com>
2021-09-09 17:42:45 -0400

    Fixed some errors with handlebars, website works

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const hbs = require('handlebars');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
@@ -14,8 +15,8 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
-// register handlebars partials
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+// register handlebars partials (https://www.npmjs.com/package/hbs) 
+hbs.unregisterPartial(path.join(__dirname, 'app_server', 'views/partials'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.router();
-const controller = require('../controllers/travel');
-
-/*GET homepage */
-router.get('/', controller.travel);
-
+const express = require('express'); 
+const router = express.Router(); 
+const controller= require('../controllers/travel'); 
+ 
+/* GET home page. */ 
+router.get('/', controller.travel); 
+ 
 module.exports = router;
